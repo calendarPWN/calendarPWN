eb42d327365865eda230fca29239cd2588d8241f
Wojciech_Staniszewski <wost91@gmail.com>
2018-04-15 14:30:40 +0200

    wokow

---
+++ src/main/java/pl/pwn/reaktor/controller/LoginController.java
@@ -0,0 +1,49 @@
+package pl.pwn.reaktor.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
+import pl.pwn.reaktor.Main;
+import pl.pwn.reaktor.service.UserService;
+
+public class LoginController {
+
+    @FXML
+    private Button btn_login;
+
+    @FXML
+    private TextField tf_login;
+
+    @FXML
+    private TextField tf_password;
+
+    @FXML
+    void login_action(MouseEvent event) {
+    	String login = tf_login.getText();
+		String password = tf_password.getText();
+		boolean isLogin = userService.login(login, password);
+		if (isLogin) {
+			Parent parent = FXMLLoader.load(getClass().getResource("/view/AnkietaView.fxml"));
+			Scene scene = new Scene(parent);
+			Main.getPrimaryStage().setScene(scene);
+		} else {
+			Alert error = new Alert(AlertType.ERROR);
+			error.setHeaderText("Error");
+			error.setContentText("Enter valid login/password");
+			error.setTitle("Wrong login or password");
+			error.show();
+		}
+    }
+    private UserService userService;
+
+	public void initialize() {
+		userService = new UserService();
+	}
+
+}
